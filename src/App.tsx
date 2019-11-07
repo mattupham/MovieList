acf9c49813d14fd0a1f4d2e585ccf73f7be2b837
Matt Upham <mcupham@gmail.com>
2019-11-06 21:57:42 -0800

    Added config settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,31 @@
 import React from 'react';
-import logo from 'src/logo.svg';
 import 'src/App.css';
 
 interface Movie {
-  title: string
+  title: string;
 }
 
-const movies: Movie[]  = [
-  {title: 'Mean Girls'},
-  {title: 'Hackers'},
-  {title: 'The Grey'},
-  {title: 'Sunshine'},
-  {title: 'Ex Machina'},
+const movies: Movie[] = [
+  { title: 'Mean Girls' },
+  { title: 'Hackers' },
+  { title: 'The Grey' },
+  { title: 'Sunshine' },
+  { title: 'Ex Machina' },
 ];
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Movie List</header>
       <div>
         <ul>
           {movies.map(({ title }: Movie) => {
-            <li>{title}</li>
+            <li>{title}</li>;
           })}
         </ul>
       </div>
     </div>
   );
-}
+};
 
 export default App;
