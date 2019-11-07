b8dfbad6d907e4161afc0c5c98826091ba3ccb3b
Matt Upham <mcupham@gmail.com>
2019-11-06 22:50:15 -0800

    Add watched toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,15 @@ import AddMovie from './components/AddMovie/AddMovie';
 
 interface Movie {
   title: string;
+  watched: boolean;
 }
 
 const movieList: Movie[] = [
-  { title: 'Mean Girls' },
-  { title: 'Hackers' },
-  { title: 'The Grey' },
-  { title: 'Sunshine' },
-  { title: 'Ex Machina' },
+  { title: 'Mean Girls', watched: false },
+  { title: 'Hackers', watched: false },
+  { title: 'The Grey', watched: false },
+  { title: 'Sunshine', watched: false },
+  { title: 'Ex Machina', watched: false },
 ];
 
 const App: React.FC = () => {
@@ -23,7 +24,9 @@ const App: React.FC = () => {
     <div className="App">
       <header>Movie List</header>
       <div>
-        <AddMovie handleAddMovie={(movieToAdd: string) => setMovies(movies.concat({ title: movieToAdd }))} />
+        <AddMovie
+          handleAddMovie={(movieToAdd: string) => setMovies(movies.concat({ title: movieToAdd, watched: false }))}
+        />
       </div>
       <div>
         <Search handleQuerySubmit={(query: string) => setSearchQuery(query)} />
@@ -32,9 +35,19 @@ const App: React.FC = () => {
         <ul>
           {movies
             .filter(movie => movie.title.toLowerCase().includes(searchQuery.toLowerCase()))
-            .map(({ title }: Movie, index: number) => (
+            .map(({ title, watched }: Movie, index: number) => (
               <li className={styles.movieItem} key={index}>
-                {title}
+                <div>{title}</div>
+                <button
+                  onClick={() => {
+                    const newMovies = [...movies];
+                    newMovies[index].watched = !newMovies[index].watched;
+                    setMovies(newMovies);
+                  }}
+                  style={{ backgroundColor: watched ? 'green' : 'red' }}
+                >
+                  {watched ? 'Watched' : 'To Watch'}
+                </button>
               </li>
             ))}
         </ul>
